e8b5764dbd401f1555f2e68429af70eecfa9d5e1
liuguofeng <liuguofeng@diligrp.com>
2015-12-23 18:10:56 +0800

    order

---
+++ app/src/main/java/com/ticket/ui/activity/OrderDetailsActivity.java
@@ -0,0 +1,23 @@
+package com.ticket.ui.activity;
+
+import android.view.View;
+
+import com.ticket.ui.base.BaseActivity;
+
+public class OrderDetailsActivity extends BaseActivity {
+
+    @Override
+    protected int getContentViewLayoutID() {
+        return 0;
+    }
+
+    @Override
+    protected View getLoadingTargetView() {
+        return null;
+    }
+
+    @Override
+    protected void initViewsAndEvents() {
+
+    }
+}
