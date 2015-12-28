901423427b0cf051a690375b39c4f429dfe9c529
liuguofeng <liuguofeng@diligrp.com>
2015-12-28 18:46:00 +0800

    修复定时器问题

--- app/src/main/java/com/ticket/ui/activity/RestAndForgotActivity.java
+++ app/src/main/java/com/ticket/ui/activity/RestAndForgotActivity.java
@@ -108,6 +108,7 @@ public class RestAndForgotActivity extends BaseActivity {
                                         public void onResponse(Response<UserVo> response, Retrofit retrofit) {
                                             if (response.isSuccess() && response.body() != null && response.body().isSuccessfully()) {
                                                 AppPreferences.putString("userId", response.body().getUserId());
+                                                countTimer.cancel();
                                                 readyGoThenKill(HomeActivity.class);
                                             } else {
                                                 CommonUtils.make(RestAndForgotActivity.this, response.body().getErrorMessage());
