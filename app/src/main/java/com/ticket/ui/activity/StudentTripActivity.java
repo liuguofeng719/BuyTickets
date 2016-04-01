9be9dcea031a716d600e472bf49e149bc352a227
liuguofeng <liuguofeng@diligrp.com>
2016-04-01 18:29:45 +0800

    增加学生出行和tab切换样式

---
+++ app/src/main/java/com/ticket/ui/activity/StudentTripActivity.java
@@ -0,0 +1,42 @@
+package com.ticket.ui.activity;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ticket.R;
+import com.ticket.ui.base.BaseActivity;
+
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+/**
+ * 学生出行
+ */
+public class StudentTripActivity extends BaseActivity {
+
+    @InjectView(R.id.btn_back)
+    ImageView btn_back;
+    @InjectView(R.id.tv_header_title)
+    TextView tv_header_title;
+
+    @OnClick(R.id.btn_back)
+    public void btnBack() {
+        finish();
+    }
+
+    @Override
+    protected int getContentViewLayoutID() {
+        return R.layout.student_trip;
+    }
+
+    @Override
+    protected View getLoadingTargetView() {
+        return null;
+    }
+
+    @Override
+    protected void initViewsAndEvents() {
+        tv_header_title.setText("学生出行");
+    }
+}
